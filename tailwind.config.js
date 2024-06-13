574fa7554ccdecebdf0e39fe5e2411423407eadc
wookyung <wk.nam.dev@gmail.com>
2024-06-13 21:27:02 +0900

    tailwindcss_just in time compiler(jit) 학습_24.06.13

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,6 @@ module.exports = {
   theme: {
     extend: {},
   },
-  darkMode: "class",  // media - 브라우저의 설정을 따라감, class - 브라우저 설정과 상관없이 모드를 토글해야 함
+  darkMode: "media",  // media - 브라우저의 설정을 따라감, class - 브라우저 설정과 상관없이 모드를 토글해야 함
   plugins: [],
 }
